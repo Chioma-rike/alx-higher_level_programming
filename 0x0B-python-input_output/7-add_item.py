e291c8d99c169b5f00785e62865f14c84cf8bed1
chioma-rike <chiomaezurike89@gmail.com>
2024-02-20 14:59:57 -0800

    Python - Input/Output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-"""Creates a pythn list from command line arguments and saves as JSON file"""
-
+"""Save argument to a file."""
 import sys
-import json
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-save_file = "add_item.json"
-try:
-    json_list = load_from_json_file(save_file)
-except Exception:
-    json_list = []
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-if len(sys.argv) > 1:
-    for index, arg in enumerate(sys.argv):
-        if index > 0:
-            json_list.append(arg)
-
-save_to_json_file(json_list, save_file)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
