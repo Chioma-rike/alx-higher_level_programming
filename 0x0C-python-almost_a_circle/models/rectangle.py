5e4487434a1a52ac3e611f94ba9194f76fa3a7c1
chioma-rike <chiomaezurike89@gmail.com>
2024-04-17 17:39:14 -0700

    correction

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -131,4 +131,4 @@ class Rectangle(Base):
 
     def __str__(self):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
-                                                       self.width, self.height
+                                                       self.width, self.height)
