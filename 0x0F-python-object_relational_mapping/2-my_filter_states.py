a0a12759949c114c8f6ab92d360ce0d7addb95e3
chioma-rike <chiomaezurike89@gmail.com>
2024-04-29 16:17:22 -0700

    tasks

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""  display all table in a state where name matched the arg """
-import MySQLdb
+""" This is a script that uses MySQLdb to lists all states from
+the database hbtn_0e_0_usa that matches the passes argument keyword.
+"""
+
 import sys
+import MySQLdb
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    c = db.cursor()
-    c.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
-    rows = c.fetchall()
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
+id ASC".format(sys.argv[4])
+    cur.execute(query)
+    rows = cur.fetchall()
     for row in rows:
         print(row)
-    c.close()
+    cur.close()
     db.close()
